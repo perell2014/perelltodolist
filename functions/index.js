1a78faadc7f81e0b1f183c641533eda674128a06
perell2014 <perell2014@users.noreply.github.com>
2025-11-15 01:05:27 +0100

    index.js env

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 export async function onRequest({ env }) {
-  const key = env.GOOGLE_CLIENT_ID; // your secret
-  return new Response("worker ok: " + Boolean(key) +  " Value: " + key);
+  //const key = env.GOOGLE_CLIENT_ID; // your secret
+  //return new Response("worker ok: " + Boolean(key) +  " Value: " + key);
+  return new Response(
+      JSON.stringify({
+        GOOGLE_CLIENT_ID: env.GOOGLE_CLIENT_ID,
+        JSONBIN_BIN_ID: env.JSONBIN_BIN_ID,
+        JSONBIN_MASTER_KEY: env.JSONBIN_MASTER_KEY,
+         })
+         );
 }
 
